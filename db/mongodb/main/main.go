4abb813faf5b71d5238e647702a446e9195085d2
LvQiang1234 <1516305754@qq.com>
2021-10-23 15:06:53 +0800

    mongo

---
+++ db/mongodb/main/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/mgo.v2"
+	"mynet/db/mongodb"
+)
+
+func main() {
+	c, err := mongodb.Dial("192.168.48.178", 10)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer c.Close()
+
+	// session
+	s := c.Ref()
+	defer c.UnRef(s)
+	err = s.DB("test").C("counters").RemoveId("test")
+	if err != nil && err != mgo.ErrNotFound {
+		fmt.Println(err)
+		return
+	}
+
+	// auto increment
+	err = c.EnsureCounter("test", "counters", "test")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for i := 0; i < 3; i++ {
+		id, err := c.NextSeq("test", "counters", "test")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(id)
+	}
+
+	// index
+	c.EnsureUniqueIndex("test", "counters", []string{"key1"})
+
+	// Output:
+	// 1
+	// 2
+	// 3
+}
